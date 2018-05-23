ba5958a85e48fa35a1a68468898060af13ef4882
Chabanovskiy Artem <chabanovskiy.artem@gmail.com>
2018-05-24 01:03:09 +0300

    added network temlate, tested two 'main' configs

---
+++ DiplomaProject/Helpers/MatrixHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomaProject.Helpers
+{
+    internal static class MatrixHelper
+    {
+        public static float[][] MatCreate(int rows, int cols)
+        {
+            float[][] result = new float[rows][];
+            for (int i = 0; i < rows; ++i)
+                result[i] = new float[cols];
+            return result;
+        }
+
+        public static float[][] MatFromArray(float[] arr, int rows, int cols)
+        {
+            float[][] result = MatCreate(rows, cols);
+            int k = 0;
+            for (int i = 0; i < rows; ++i)
+            for (int j = 0; j < cols; ++j)
+                result[i][j] = arr[k++];
+            return result;
+        }
+
+        public static float[][] MatTanh(float[][] m)
+        {
+            int rows = m.Length; int cols = m[0].Length;
+            float[][] result = MatCreate(rows, cols);
+            for (int i = 0; i < rows; ++i) // Each row
+            for (int j = 0; j < cols; ++j) // Each col
+                result[i][j] = Tanh(m[i][j]);
+            return result;
+        }
+
+        //multiplies corresponding values in two matrices that have the same shape
+        public static float[][] MatHada(float[][] a, float[][] b)
+        {
+            int rows = a.Length; int cols = a[0].Length;
+            float[][] result = MatCreate(rows, cols);
+            for (int i = 0; i < rows; ++i)
+            for (int j = 0; j < cols; ++j)
+                result[i][j] = a[i][j] * b[i][j];
+            return result;
+        }
+
+        private static float Tanh(float x)
+        {
+            if (x < -10.0) return -1.0f;
+            if (x > 10.0) return 1.0f;
+            return (float)(Math.Tanh(x));
+        }
+
+        private static float Sigmoid(float x)
+        {
+            if (x < -10.0) return -1.0f;
+            if (x > 10.0) return 1.0f;
+            float k = (float)Math.Exp(-x);
+            return 1 / (1 + k);
+        }
+    }
+}
--- DiplomaProject/Program.cs
+++ DiplomaProject/Program.cs
@@ -4,11 +4,14 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DiplomaProject.Exapmles;
 using DiplomaProject.Helpers;
 using Newtonsoft.Json.Linq;
 using DiplomaProject.Mongo.Connection;
+using DiplomaProject.Mongo.Models;
 using MongoDB.Bson;
 using MongoDB.Driver;
+using ServiceStack.Redis;
 
 namespace DiplomaProject
 {
@@ -16,10 +19,71 @@ namespace DiplomaProject
     {
         private static readonly MongoConnectionClass DbClass = new MongoConnectionClass();
 
+        /*
         private static void Main(string[] args)
         {
-            var test = DataHelper.GetAndProcessData(DbClass, 1519770787);
-            Console.WriteLine("dsds");
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Invalid amount of parameters");
+            }
+
+            long result0;
+            long result1;
+
+            bool firstArgParse = long.TryParse(args[0], out result0);
+            bool secondArgParse = long.TryParse(args[1], out result1);
+
+            bool successParce = firstArgParse && secondArgParse;
+            if (!successParce || (result0 > result1))
+            {
+                Console.WriteLine("Invalid arguments");
+                return;
+            }
+
+            List<ProcessedLogInfo> resultList = DataHelper.GetAndProcessData(DbClass, result0, result1);
+
+            if (!resultList.Any())
+            {
+                Console.WriteLine("No data on this dates");
+                return;
+            }
+
+            Console.WriteLine(resultList.Count() % 2 == 0 ? "normal state of system" : "anomaly state");
+        }
+        */
+
+        private static void Main(string[] args)
+        {
+
+            using (var redisConsumer = new RedisClient(Environment.GetEnvironmentVariable("redis") ?? "localhost"))
+            using (IRedisSubscription subscription = redisConsumer.CreateSubscription())
+            {
+                subscription.OnSubscribe = channel =>
+                {
+                    Console.WriteLine($"Subscribed to '{channel}'");
+                };
+                subscription.OnUnSubscribe = channel =>
+                {
+                    Console.WriteLine($"UnSubscribed from '{channel}'");
+                };
+                subscription.OnMessage = (channel, msg) =>
+                {
+                    JToken obj = JToken.Parse(msg);
+
+                    List<ProcessedLogInfo> resultList = 
+                                    DataHelper.GetAndProcessData(DbClass, Convert.ToInt64(obj["date_low"]), Convert.ToInt64(obj["date_upper"]));
+
+                    if (!resultList.Any())
+                    {
+                        Console.WriteLine("No data on this dates");
+                        return;
+                    }
+
+                    Console.WriteLine(resultList.Count % 2 == 0 ? "normal state of system" : "anomaly state");
+                };
+
+                subscription.SubscribeToChannels("main");
+            }
         }
     }
 }
